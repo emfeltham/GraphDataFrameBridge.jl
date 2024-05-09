084dd524df6894ccdbec504b154336aa22619fc8
Eric Martin Feltham <eric.feltham@yale.edu>
2024-05-09 10:26:14 -0400

    update DataFrame to handle types

--- src/DataFrame.jl
+++ src/DataFrame.jl
@@ -18,31 +18,39 @@ function DataFrame(gr::T; type = :node) where T <:AbstractMetaGraph
         :node => Int[], gr.vprops, vertices, nv
     elseif type == :edge
         :edge => Edge[], gr.eprops, edges, ne
-      else
-        error("specify type equal to :node or :edge")
+    else
+        error("You must specify type as :node or :edge")
     end
 
     dx = DataFrame(fl)
 
-    x = unique(reduce(vcat, values(prps)))
-    for y in x
-        for (k, v) in y
-            dx[!, k] = typeof(v)[] # may be a problem if type is not consistent in y?
-            allowmissing!(dx, k)
+    # this block only applies if there are defined properties
+    # on the MetaGraph object `gr`
+    if length(values(prps)) > 0
+        x = unique(reduce(vcat, values(prps)))
+        for y in x
+            for (k, v) in y
+                if typeof(v) != Missing # update if there are non-missing entries
+                    dx[!, k] = typeof(v)[]
+                end
+                if string(k) ∈ names(dx)
+                    allowmissing!(dx, k)
+                end
+            end
         end
     end
     
     dx = similar(dx, nu(gr))
 
     for (i, e) in (enumerate∘en)(gr)
+
         dx[i, type] = e
         pr = props(gr, e)
         for (nme, val) in pr
             dx[i, nme] = val
         end
     end
-
-    # remove missing when possible
+    
     for v in Symbol.(names(dx))
         if !any(ismissing.(dx[!, v]))
             disallowmissing!(dx, v)
